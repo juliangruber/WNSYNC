946d604bea7f0e11b9154c63ac60d952e8f2e3ce
Julian Gruber <julian@juliangruber.com>
2012-06-20 06:20:49 +0200

    parse configuration file and comments

--- wnsync.js
+++ wnsync.js
@@ -17,8 +17,12 @@ Delete: (rename,null)
 	  , filesChanged = 0
 	  , syncPending = false
 	  , isSyncing = false
-	  , cmd;
+	  , cmd
+	  , cfg;
 
+	/**
+	 * Determin cmd based on OS
+	 */
 	if (process.env.OS == "Windows_NT") {
 		cmd = path.join('rsync','rsync.exe');
 	} else {
@@ -26,18 +30,52 @@ Delete: (rename,null)
 	}
 
 
+	/**
+	 * Load configuration file
+	 */
+	fs.stat('.wnsync.json', function(err, stats) {
+		if ('object' == typeof stats) {
+			cfg = require('./.wnsync.json');
+			if ("string" != typeof cfg.dir) cfg.dir = '/home/'+cfg.user+'/';
+			console.log(cfg);
+		} else {
+			throw new Error('Configuration file .wnsync.json not found')
+		}
+	})
+
+	/**
+	 * Execute rsync
+	 */
 	var sync = function() {
 		console.log('sync');
 
 		child_process.exec(cmd, function(error, stdout, stderr) {
-			console.log(stdout);
-			console.log(stderr);
+			//console.log(stdout);
+			//console.log(stderr);
 			isSyncing = false;
 		});
 	}
 
+	/**
+	 * Perform initial synchronisation
+	 */
 	sync();
 
+	/**
+	 * Only start syncing if no changes happened for 100ms
+	 * because
+	 * - Complex FS events like `change` consist of more than
+	 *   one event
+	 * - More than one file could be changes, we then want
+	 *   to do a batch sync
+	 *
+	 * `filesChanges` is reset after 100ms to see if fs.watch
+	 * incremented it again.
+	 *
+	 * `syncPending` is needed because sync should happen if
+	 * filesChanges is 0, but filesChanges is also 0 when
+	 * nothing happened -> 2nd status variable
+	 */
 	setInterval(function() {
 		if (syncPending && filesChanged == 0) {
 			sync();
@@ -46,6 +84,9 @@ Delete: (rename,null)
 		filesChanged = 0;
 	}, 100);
 
+	/**
+	 * Watch the file system for changes
+	 */
 	fs.watch('./', function(event, path) {
 		filesChanged++;
 		syncPending = true;
